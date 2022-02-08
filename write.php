b234b41cb895a2ab1e87355b46892da4092dab82
はんどくりーむ <99105005+handcreamde@users.noreply.github.com>
2022-02-08 17:32:30 +0900

    Update write.php

--- write.php
+++ write.php
@@ -2,10 +2,10 @@
     $text = file_get_contents('text.php');
     $name = strtr($_POST["name"], "<", "＜");
     $name = strtr($name, ">", "＞");
-    $get = strtr($_POST["lore"], "<br>", "φ");
+    $get = strtr($_POST["lore"], "<br>", "$");
     $get = strtr($get, ">", "＞");
     $get = strtr($get, "<", "＜");
-    $get = strtr($get, "φ", "<br>");
+    $get = strtr($get, "$", "<br>");
     $kakikaki = '<p><font size="5">'.$name.'</font><br>'.$get.'</p>'.$text;
     file_put_contents("text.php", $kakikaki);
 ?>
